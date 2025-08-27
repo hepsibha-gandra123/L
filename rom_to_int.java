311861bd202b7e6963a2b237c8a904d376d7cb2e
Hepsibha <hepsiba17116@gmail.com>
2025-08-27 14:11:29 +0530

    updaetd solution for rom_to_int

--- rom_to_int.java
+++ rom_to_int.java
@@ -78,3 +78,32 @@ class Solution {
         return s.toString();
     }
 }
+
+
+//chatgpt solution
+
+class Solution {
+    public String intToRoman(int num) {
+        StringBuilder s = new StringBuilder();
+        
+        // Define arrays for Roman numeral symbols and their corresponding values
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        
+        // Extract the digits for thousands, hundreds, tens, and units
+        int thou = num / 1000;
+        int hund = (num % 1000) / 100;
+        int ten = (num % 100) / 10;
+        int unit = num % 10;
+        
+        // Build the Roman numeral string
+        s.append(thousands[thou]);
+        s.append(hundreds[hund]);
+        s.append(tens[ten]);
+        s.append(units[unit]);
+        
+        return s.toString();
+    }
+}
