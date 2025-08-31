b549b46d315f55db0dc45beac7db354405594be9
Hepsibha <hepsiba17116@gmail.com>
2025-08-31 10:48:59 +0530

    finding sqrt of a number

---
+++ sqrt.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0||x==1){
+            return x;
+        }
+        int low=1;
+        int high=x;
+        int sqrt=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(mid==x/mid){
+                return mid;
+            }
+            else if(mid<x/mid){
+                sqrt=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return sqrt;
+        
+    }
+}
