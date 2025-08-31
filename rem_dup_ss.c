95991c6a6d202cfa125df5b607c373eeb625230d
Hepsibha <hepsiba17116@gmail.com>
2025-08-31 10:52:26 +0530

    removing duplicates in single linked list

---
+++ rem_dup_ss.c
@@ -0,0 +1,14 @@
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    struct ListNode* current=head;
+    while(current!=NULL &&            current->next!=NULL){
+        if(current->val==current->next->val){
+            struct ListNode* duplicate=current->next;
+            current->next=current->next->next;
+            free(duplicate);
+        }
+        else{
+            current=current->next;
+        }
+    }
+    return head;
+}
